cb50d661aedcb829e10e7defd4ff9ffa838756f9
Alexandre <alexandrefsoficial@gmail.com>
2023-01-25 20:06:27 -0300

    auth Router fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv";
 import cors from "cors"
-import AuthRoutes from "./routes/AuthRoutes.js"
+import authRoutes from "./routes/AuthRoutes.js"
 
 dotenv.config();
 
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use([AuthRoutes])
+app.use([authRoutes])
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Servidor funcionando na porta: ${port}`));
