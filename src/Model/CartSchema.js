84e6f42b55b405dfd8c9d91c2212d219507bb38b
Alexandre <alexandrefsoficial@gmail.com>
2023-01-25 20:38:23 -0300

    create cart method implemented

--- src/Model/CartSchema.js
+++ src/Model/CartSchema.js
@@ -1,5 +1,6 @@
 import joi from 'joi'
 
 export const cartSchema = joi.object({
-    userId: joi.string().required()
+    sessionId: joi.string().required(),
+    chosenItems: joi.array()
 });
--- src/controller/Cart.js
+++ src/controller/Cart.js
@@ -3,13 +3,32 @@ import { v4 as uuidV4 } from 'uuid';
 import db from '../config/database.js';
 import { cartSchema } from '../Model/CartSchema.js';
 
-export async function getNumberOfItems(req,res){
-  const { userId } = req.body;
+export async function getNumberOfItems(req, res) {
+    const { sessionId } = req.body;
 
-  try {
-    const cart = await db.collection("carts").findOne({ userId })
-    res.status(200).send(cart.chosenItems.length)
-  } catch (error) {
-    res.status(500).send(error.message)
-  }
+    try {
+        const cart = await db.collection("carts").findOne({ sessionId })
+        res.status(200).send(cart.chosenItems.length)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function createCart(req, res) {
+    const { sessionId, chosenItems } = req.body;
+
+    const { error } = cartSchema.validate({ sessionId, chosenItems })
+
+    if (error) {
+        const errorMessages = error.details.map(err => err.message)
+        return res.status(422).send(errorMessages)
+    }
+
+    try {
+        await db.collection("carts").insertOne({ sessionId, chosenItems })
+        res.status(201)
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 }
