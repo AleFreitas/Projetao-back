a6bdefd44a311f8220f38b3688216ae87621c98f
Alexandre <alexandrefsoficial@gmail.com>
2023-01-28 13:53:44 -0300

    create cart removed

--- src/routes/CartRoutes.js
+++ src/routes/CartRoutes.js
@@ -1,11 +1,10 @@
-import { getNumberOfItems, createCart, postItem, removeItem } from '../controller/Cart.js';
+import { getNumberOfItems, postItem, removeItem } from '../controller/Cart.js';
 import { Router } from 'express';
 
 const cartRouter = Router()
 
 // Rotas de autenticação
 cartRouter.get("/number-of-items", getNumberOfItems)
-cartRouter.post("/create-cart", createCart)
 cartRouter.post("/post-item", postItem)
 cartRouter.post("/remove-item", removeItem)
 
