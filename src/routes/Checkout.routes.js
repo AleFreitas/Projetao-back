bc4b476c745d665984775be3e9d9fc07ce1f3808
Rennan <reiony.dev@gmail.com>
2023-01-30 18:02:59 -0300

    updated route checkout

--- src/routes/Checkout.routes.js
+++ src/routes/Checkout.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { checkOut } from '../controllers/Checkout.controller.js';
 import { authValidation } from '../middlewares/AuthValidation.middleware.js';
+import { CheckoutPostValidation } from "../middlewares/CheckoutOrder.middleware.js"
 
 const checkoutRoutes = Router();
 checkoutRoutes.use(authValidation);
-checkoutRoutes.post("/checkout", checkOut);
+checkoutRoutes.post("/checkout", CheckoutPostValidation, checkOut);
 
 export default checkoutRoutes
